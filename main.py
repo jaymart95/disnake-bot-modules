112260e6c2e14d2387eee41d58d8bcfdccfff257
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2022-06-24 23:15:10 -0500

    moved to poetry

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
-'''
+"""
 A simple, boiler plate main.py necessary to only run the bot and load the cogs (modules)
-'''
+"""
 
-from os import getenv, listdir, path
+from os import getenv, listdir
 
 from disnake import Intents
 from disnake.ext import commands
 from dotenv import load_dotenv
 
-'''
+from modules.teams import Teams
+from modules.trivia import Trivia
+
+"""
 Add test_guilds = [guildID] as a parameter to commands.Bot if you
 with to bypass global slash command registration while testing.
 
 Example
 -------
 bot = commands.Bot(intents=intents, test_guilds=[1234567890])
-'''
+"""
 
 # Intialize bot and declare intents
 intents = Intents.all()
-bot = commands.Bot(intents=intents)
-
+bot = commands.Bot(
+    intents=intents, test_guilds=[878687437965099018, 947543739671412878]
+)
 
 
 @bot.listen()
 async def on_ready():
-    print('------------------------------------------------------------------')
-    print(f'{bot.user.name} is connected to Discord and listening for events.')
+    print("------------------------------------------------------------------")
+    print(f"{bot.user.name} is connected to Discord and listening for events.")
 
 
-
-def load_cogs(bot):
-    '''load cogs (.py) files location in /cogs'''
-    for file in listdir('./cogs'):
-        if file.endswith('.py'):
-            bot.load_extension(f'cogs.{file[:-3]}')
+# load bot modules
+bot.add_cog(Teams(bot))
+bot.add_cog(Trivia(bot))
 
 
-
-if __name__ == '__main__':
-    load_cogs(bot)
+if __name__ == "__main__":
     load_dotenv()
-    bot.run(getenv('TOKEN'))
+    bot.run(getenv("TOKEN"))
